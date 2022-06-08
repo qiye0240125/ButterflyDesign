66ca5df6a953d99df6679799e5767c9da3ea86b7
qiye <1181362579@qq.com>
2022-06-08 11:46:03 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     我是div
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componnts/Button/button.jsx
@@ -0,0 +1,8 @@
+import React,{useState} from 'react'
+export const Button = () =>{
+    return (
+        <div>
+            <button>主要按钮</button>
+        </div>
+    )
+}
